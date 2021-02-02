05c38592f1ae152dab3c5b301994030961111ab3
Elsa Rose <47546213+emastico@users.noreply.github.com>
2021-02-01 17:15:02 -0800

    Update PlotsWellness.R
    Codes for a flipped barchart and a grouped bar chart using qualitative data

--- PlotsWellness.R
+++ PlotsWellness.R
@@ -12,6 +12,7 @@ data %>% #GraphforQualitativeData
   ggplot(aes(x=name, y=frequency, fill=as.factor(name))) + 
     geom_bar(stat="identity") +
     scale_fill_manual(values=c("#7F3C8D","#11A579","#3969AC","#F2B701","#E73F74","#80BA5A","#E68310","#008695")) +
+#ColorsareFromCARTOcolorsfromtheirBOLD
     coord_flip() +
     theme_classic() +
     xlab("Location of Wellness") +
@@ -25,6 +26,32 @@ data %>% #GraphforQualitativeData
     )
       #hjust=1:placetitleonright
       #hjust=2:placestitleonleft
+##Wordcloud Creation
 library(wordcloud2)
 attach(Buzzwords_for_Wellness_)
 wordcloud2(Buzzwords_for_Wellness_, size=1.6, color='random-dark')
+#Grouped Bargraph Creation
+#CreateSubset 
+public<-subset(Wellness_Research_Mastico_Numerical_Version, `Type of College`=="1")
+private<-subset(Wellness_Research_Mastico_Numerical_Version, `Type of College`=="2")
+#CreateTables
+PublicWellness<- table(public$`Wellness Definition`)
+PrivateWellness<- table(private$`Wellness Definition`)
+#createDataFramesformoredetail
+df2 <- data.frame(college=rep(c("Private", "Public"), each=5),
+       def=rep(c("Holistic","Mind and Body","Unclear","Physical Only","Unmet Community Needs"),2),
+       freq=c(19,43,2,30,0,17,51,3,31,2))
+#createbargraph
+ggplot(data=df2, aes(x=def, y=freq, fill=college)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  scale_fill_manual(values=c("#E68310","#008695")) +
+  theme_classic() +
+  xlab("Wellness Definition") +
+  ylab("Frequency") +
+  labs(fill="Legend", 
+       title = "Public vs. Private University Wellness Definitions", 
+       caption="Graph Created in R Studio") +
+  theme(
+    plot.title = element_text(hjust=0.5, lineheight=0.9),
+    plot.caption=element_text(color="black", face="italic")
+  )
